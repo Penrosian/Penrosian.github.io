82c4c6dbf5e4028ce60e4fba0fc6d4316f584e62
Benjamin Slatt <benjamin@dodgersden.com>
2024-11-26 14:41:53 -0500

    Add stars & allow for more changes using variables.

--- js-turtle-master/turtle.js
+++ js-turtle-master/turtle.js
@@ -7,80 +7,58 @@ right(angle)
 left(angle) 	   
 goto(x,y) 	       
 clear() 	       
-penup() 	       
-pendown() 	       
+hide() 	       
+show() 	       
 reset() 	       
 angle(angle)	   
-width(width)       
-
+setSize(width) 
 color(r,g,b)
 color([r,g,b])
 color("red")
 color("#ff0000")
 */
 
-//Functions
-function square(length) {
-    pendown()
-    for (let i = 0; i < 4; i++) {
-        forward(length)
-        right(90)
-    }
-    penup()
-}
-function etriangle(length) {
-    pendown()
-    for (let i = 0; i < 3; i++) {
-        forward(length)
-        right(120)
-    }
-    penup()
-}
-function star(radius,lines) {
-    rot = 360/lines
-    pendown()
-    for (let i = 0; i < lines; i++) {
-        forward(radius)
-        right(180)
-        forward(radius)
-        left(180-rot)
-    }
-    penup()
-}
-/* For loop 
-for (let i = 0; i < len; i++) {
+//Definitions
+let bg = "gray"
+let main = "white"
+let detail = "black"
+let size = 5
 
-}
-*/
+//For loop 
+// for (let i = 0; i < len; i++) {}
 
 //Setup
 showGrid(50)
-setSpeed(1) //Small = Fast
-color("black")
-width(600)
+setSpeed(-1) //Small = Fast
+color(bg)
+setSize(600)
 goto(0,-300)
 forward(600)
-penup()
+hide()
 
 //Begin
-color("blue")
-width(1)
-goto(-275,-300)
-for (let i = 0; i < 12; i++) {
-    forward(25)
-    star(5,8)
+setSize(1)
+goto(-275,-275)
+color(detail)
+star(5, 8)
+for (let i = 0; i < 11; i++) {
+    forward(50)
+    star(5, 8)
+}
+for (let i = 0; i < 11; i++) {
+    right(180)
+    forward(550)
+    left(90)
+    forward(50)
+    left(90)
+    star(5, 8)
     for (let i = 0; i < 11; i++) {
         forward(50)
         star(5, 8)
     }
-    right(180)
-    forward(575)
-    left(90)
-    forward(50)
-    left(90)
 }
-width(4)
-color("red")
+setSize(4)
+color(main)
 goto(-300,-300)
 for (let i = 0; i < 4; i++) {
     for (let i = 0; i < 12; i++) {
@@ -100,8 +78,8 @@ for (let i = 0; i < 4; i++) {
     forward(600)
     right(90)
 }
-width(5)
-color("black")
+setSize(5)
+color(bg)
 goto(-300,-300)
 for (let i = 0; i < 12; i++) {
     for (let i = 0; i < 12; i++) {
