49422741dc4e1e14d845e4e9243dff14be473c3b
Benjamin Slatt <benjamin@dodgersden.com>
2025-01-13 12:07:20 -0500

    Add debug

--- src/js/init.js
+++ src/js/init.js
@@ -77,10 +77,12 @@ function renderValidationResults(data) {
     ValidatorHTML += ` Valid!</strong></p>`;
     if(
         (window.location.href.split(".html").length - 1 == 0 & window.location.href.split("/").length - 1 == 0) || 
-        (window.location.href.split(".html").length - 1 != 0 & window.location.href.split("/").length - 1 <= 1)
+        (window.location.href.split(".html").length - 1 > 0 & window.location.href.split("/").length - 1 <= 1)
     ) {
         ValidatorHTML += `
         <p>
+            Debug: .html ${window.location.href.split(".html").length}
+            Debug: / ${window.location.href.split("/").length}
             <a id="vLink1" href="https://validator.w3.org/check?uri=${window.location.href}">Validate HTML</a> |
             <a id="vLink2" href="https://jigsaw.w3.org/css-validator/validator?uri=${window.location.href}?profile=css3">Validate CSS</a>
             <a id="vLink2" href="https://jigsaw.w3.org/css-validator/validator?uri=${window.location.href}?profile=css3">Validate CSS</a>
@@ -91,7 +93,6 @@ function renderValidationResults(data) {
         <p>
             <a id="vLink1" href="https://validator.w3.org/check?uri=${window.location.href}">Validate HTML</a> |
             <a id="vLink2" href="https://jigsaw.w3.org/css-validator/validator?uri=${window.location.href}?profile=css3">Validate CSS</a> | 
-            <a id="vLink3" href="../">Go Back</a>
             <a id="vLink3" href="../">Go Back</a>
         </p>
     `;
