186e6104af8629f84959774178611bfc9c288040
Benjamin Slatt <benjamin@dodgersden.com>
2025-03-28 11:23:42 -0400

    Add mindustry app script

---
+++ imghost/minapp.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd $HOME
+# Make the app
+mkdir Desktop/Mindustry.app
+mkdir Desktop/Mindustry.app/Contents
+mkdir Desktop/Mindustry.app/Contents/MacOS
+mkdir Desktop/Mindustry.app/Contents/Resources
+# Start script
+printf "#\!/bin/bash\ncd $HOME/Mindustry\n./gradlew desktop:run" > Desktop/Mindustry.app/Contents/MacOS/Mindustry
+chmod +x Desktop/Mindustry.app/Contents/Macos/Mindustry
+# info.plist for icon
+printf "<?xml version=%q1.0%q encoding=%qUTF-8%q?>\n<\!DOCTYPE plist PUBLIC%q-//Apple//DTD PLIST 1.0//EN%q %qhttp://www.apple.com/DTDs/PropertyList-1.0.dtd%q>\n<plist version=%q1.0%q>\n  <dict>\n    <key>CFBundleIconFile</key>\n    <string>minIcon</string>\n  </dict>\n</plist>" > Desktop/Mindustry.app/Contents/info.plist
+# Download icon 
+curl --output Desktop/Mindustry.app/Contents/Resources/minIcon.icns https://penrosian.github.io/imghost/minIcon.icns
