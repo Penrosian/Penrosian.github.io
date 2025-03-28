226b51d5d01bc6599adaa865929a296e7d77e408
Benjamin Slatt <benjamin@dodgersden.com>
2025-03-28 12:01:12 -0400

    Update minapp script

--- imghost/minapp.sh
+++ imghost/minapp.sh
@@ -6,9 +6,9 @@ mkdir Desktop/Mindustry.app/Contents
 mkdir Desktop/Mindustry.app/Contents/MacOS
 mkdir Desktop/Mindustry.app/Contents/Resources
 # Start script
-printf "#\!/bin/bash\ncd $HOME/Mindustry\n./gradlew desktop:run" > Desktop/Mindustry.app/Contents/MacOS/Mindustry
+printf "#!/bin/bash\ncd $HOME/Mindustry\n./gradlew desktop:run" > Desktop/Mindustry.app/Contents/MacOS/Mindustry
 chmod +x Desktop/Mindustry.app/Contents/Macos/Mindustry
 # info.plist for icon
-printf "<?xml version=%q1.0%q encoding=%qUTF-8%q?>\n<\!DOCTYPE plist PUBLIC%q-//Apple//DTD PLIST 1.0//EN%q %qhttp://www.apple.com/DTDs/PropertyList-1.0.dtd%q>\n<plist version=%q1.0%q>\n  <dict>\n    <key>CFBundleIconFile</key>\n    <string>minIcon</string>\n  </dict>\n</plist>" > Desktop/Mindustry.app/Contents/info.plist
+printf "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<\!DOCTYPE plist PUBLIC\"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">\n<plist version=\"1.0\">\n  <dict>\n    <key>CFBundleIconFile</key>\n    <string>minIcon</string>\n  </dict>\n</plist>" > Desktop/Mindustry.app/Contents/info.plist
 # Download icon 
 curl --output Desktop/Mindustry.app/Contents/Resources/minIcon.icns https://penrosian.github.io/imghost/minIcon.icns
