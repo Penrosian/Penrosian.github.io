1be847c27a95d6ec06537efa8e8db0aacaadc325
Penrosian <benjamin@dodgersden.com>
2025-04-23 09:42:27 -0400

    Formattin

--- JS/game.js
+++ JS/game.js
@@ -7,11 +7,11 @@ const canvasHeight = Number(canvas.getAttribute("height"));
 console.log(canvasWidth);
 console.log(canvasHeight);
 
-function randInt(low, high){
+function randInt(low, high) {
     return Math.floor(Math.random() * (1 + high - low)) + low;
 }
 
-function fillCircle(x, y, radius, fillColor, strokeColor, strokeWidth, arcLength){
+function fillCircle(x, y, radius, fillColor, strokeColor, strokeWidth, arcLength) {
     ctx.fillStyle = fillColor;
     ctx.strokeStyle = strokeColor;
     ctx.lineWidth = strokeWidth;
@@ -21,12 +21,12 @@ function fillCircle(x, y, radius, fillColor, strokeColor, strokeWidth, arcLength
     ctx.fill();
 }
 
-function fillPage(fillColor){
+function fillPage(fillColor) {
     ctx.fillStyle = fillColor;
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 }
 
-function fillRect(x, y, width, height, fillColor){
+function fillRect(x, y, width, height, fillColor) {
     ctx.fillStyle = fillColor;
     ctx.fillRect(x, y, width, height);
 }
@@ -76,59 +76,59 @@ function fillRect(x, y, width, height, fillColor){
 */
 
 let animData = {
-    "rects":[
+    "rects": [
         {
-            "id":"player",
-            "x":55,
-            "y":80,
-            "width":20,
-            "height":20,
-            "color":"black",
-            "animation":"bounce",
-            "xVel":5,
-            "yVel":-5
+            "id": "player",
+            "x": 55,
+            "y": 80,
+            "width": 20,
+            "height": 20,
+            "color": "black",
+            "animation": "bounce",
+            "xVel": 5,
+            "yVel": -5
         },
         {
-            "id":"ground",
-            "x":0,
-            "y":520,
-            "width":960,
-            "height":20,
-            "color":"green",
-            "animation":"static",
-            "xVel":0,
-            "yVel":0
+            "id": "ground",
+            "x": 0,
+            "y": 520,
+            "width": 960,
+            "height": 20,
+            "color": "green",
+            "animation": "static",
+            "xVel": 0,
+            "yVel": 0
         }
     ],
-    "circles":[
+    "circles": [
         {
-            "id":"ball",
-            "x":55,
-            "y":80,
-            "radius":10,
-            "length":1,
-            "animation":"bounce",
-            "xVel":5,
-            "yVel":-5,
-            "color":"blue",
-            "lineColor":"black",
-            "lineWidth":0
+            "id": "ball",
+            "x": 55,
+            "y": 80,
+            "radius": 10,
+            "length": 1,
+            "animation": "bounce",
+            "xVel": 5,
+            "yVel": -5,
+            "color": "blue",
+            "lineColor": "black",
+            "lineWidth": 0
         }
     ]
 };
 
-function animate(){
+function animate() {
     fillPage("lightBlue");
 
-    for (let i = 0; i < animData.rects.length; i++){
+    for (let i = 0; i < animData.rects.length; i++) {
         rect = animData.rects[i];
         fillRect(rect.x, rect.y, rect.width, rect.height, rect.color);
         // Different animation styles move in different ways
-        if (rect.animation == "bounce"){
+        if (rect.animation == "bounce") {
             if (rect.x >= canvasWidth - rect.width || rect.x <= 0) rect.xVel *= -1;
             if (rect.y >= canvasHeight - rect.height || rect.y <= 0) rect.yVel *= -1;
         }
-        if (rect.animation != "static"){
+        if (rect.animation != "static") {
             rect.x += rect.xVel;
             rect.y += rect.yVel;
         }
@@ -137,15 +137,15 @@ function animate(){
     }
 
     // Rectangles and circles are drawn/stored seperately
-    for (let i = 0; i < animData.circles.length; i++){
+    for (let i = 0; i < animData.circles.length; i++) {
         circle = animData.circles[i];
         fillCircle(circle.x, circle.y, circle.radius, circle.color, circle.lineColor, circle.lineWidth, circle.length)
         // Different animation styles move in different ways
-        if (circle.animation == "bounce"){
+        if (circle.animation == "bounce") {
             if (circle.x >= canvasWidth - circle.radius || circle.x <= circle.radius) circle.xVel *= -1;
             if (circle.y >= canvasHeight - circle.radius || circle.y <= circle.radius) circle.yVel *= -1;
         }
-        if (circle.animation != "static"){
+        if (circle.animation != "static") {
             circle.x += circle.xVel;
             circle.y += circle.yVel;
         }
